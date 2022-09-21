4d9ebc158203b7d04ce3cc1f9528649eb019de92
Derek Volner <89471429+DerekVolner@users.noreply.github.com>
2022-09-20 22:58:56 -0500

    java,css, and html mixed
    used functions

---
+++ scripts/script.js
@@ -0,0 +1,55 @@
+/*
+    Student Name: Derek Volner
+    File Name: script.js
+    Date: September 20th, 2022
+*/
+
+//Global variables
+var video = document.getElementById("example");
+var videoSource = document.getElementById("vid-src");
+var descriptionSource = document.getElementById("despsrc");
+
+//Hambuger menu function
+function hamburger () {
+    var menu = document.getElementyById("menu-link");
+    var logo = document.getElementById("ffc-logo");
+    if(menu.style.display === "block" && logo.style.display ==="none") {
+        menu.style.display = "none";
+        logo.style.display = "block";
+    } else {
+      menu.style.display ="block";
+      logo.style.display = "none";
+    }
+}
+
+//function to display the burpees example video
+function burpees (){
+  videoSource.src = "media/burpees.mp4";
+  descriptionSource.src = "media/burpees-descriptions.vtt";
+  video.style.display = block;
+  video.load ();
+}
+
+//Function to display the plank example videoSource
+function plank(){
+  videoSource.src = "media/plank.mp4";
+  descriptionSource.src = "media/plank-descriptions.vtt";
+  video.style.display = "block";
+  video.load();
+}
+
+//Function to display the mountain climbers example video
+function mountain(){
+  videoSource.src = "media/mc.mp4";
+  descriptionSource.src = "media/mountain-descriptions.vtt";
+  video.style.display = "block";
+  video.load();
+}
+
+//Function to display a promo Code
+function discount(){
+  var promo = document.getElementById("special");
+  promo.firstChild.nodeValue = "Promo Code: D25START";
+  promo.style.color = "#ff0000";
+  promo.style.fontSize = "2em";
+}
